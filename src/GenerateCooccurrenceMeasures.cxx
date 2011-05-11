59aba2c6be08d5f7e4665f335f23875f03dd5f67
Nick Tustison <ntustison@gmail.com>
2011-05-11 10:05:50 -0400

    ENH:  Added cooccurrence measures.

--- src/GenerateCooccurrenceMeasures.cxx
+++ src/GenerateCooccurrenceMeasures.cxx
@@ -3,62 +3,10 @@
 #include "itkImage.h"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
-#include "itkImageRegionIterator.h"
 #include "itkImageRegionIteratorWithIndex.h"
 
-#include "itkCastImageFilter.h"
-#include "itkGreyLevelCooccurrenceMatrixTextureCoefficientsCalculator.h"
-#include "itkLabelStatisticsImageFilter.h"
-#include "itkMaskedScalarImageToGreyLevelCooccurrenceMatrixGenerator.h"
-
-#include <string>
-#include <vector>
-
-template<class TValue>
-TValue Convert( std::string optionString )
-			{
-			TValue value;
-			std::istringstream iss( optionString );
-			iss >> value;
-			return value;
-			}
-
-template<class TValue>
-std::vector<TValue> ConvertVector( std::string optionString )
-			{
-			std::vector<TValue> values;
-			std::string::size_type crosspos = optionString.find( 'x', 0 );
-
-			if ( crosspos == std::string::npos )
-					{
-					values.push_back( Convert<TValue>( optionString ) );
-					}
-			else
-					{
-					std::string element = optionString.substr( 0, crosspos ) ;
-					TValue value;
-					std::istringstream iss( element );
-					iss >> value;
-					values.push_back( value );
-					while ( crosspos != std::string::npos )
-							{
-							std::string::size_type crossposfrom = crosspos;
-							crosspos = optionString.find( 'x', crossposfrom + 1 );
-							if ( crosspos == std::string::npos )
-									{
-									element = optionString.substr( crossposfrom + 1, optionString.length() );
-									}
-							else
-									{
-									element = optionString.substr( crossposfrom + 1, crosspos ) ;
-									}
-							std::istringstream iss( element );
-							iss >> value;
-							values.push_back( value );
-							}
-					}
-			return values;
-			}
+#include "itkScalarImageToTextureFeaturesFilter.h"
+#include "itkDenseFrequencyContainer2.h"
 
 template <unsigned int ImageDimension>
 int GenerateCooccurrenceMeasures( int argc, char *argv[] )
@@ -67,7 +15,6 @@ int GenerateCooccurrenceMeasures( int argc, char *argv[] )
   typedef float PixelType;
   typedef float RealType;
 
-  unsigned int numberOfBins = 200;
 
   typedef itk::Image<PixelType, ImageDimension> ImageType;
   typedef itk::Image<RealType, ImageDimension> RealImageType;
@@ -77,70 +24,48 @@ int GenerateCooccurrenceMeasures( int argc, char *argv[] )
   imageReader->SetFileName( argv[2] );
   imageReader->Update();
 
-  typedef itk::Statistics::MaskedScalarImageToGreyLevelCooccurrenceMatrixGenerator<ImageType>
-    CooccurrenceMatrixGeneratorType;
-  typename CooccurrenceMatrixGeneratorType::Pointer generator
-    = CooccurrenceMatrixGeneratorType::New();
+  typedef itk::Statistics::DenseFrequencyContainer2 HistogramFrequencyContainerType;
 
-  typename CooccurrenceMatrixGeneratorType::OffsetVectorPointer offsets
-    = CooccurrenceMatrixGeneratorType::OffsetVector::New();
+  typedef itk::Statistics::ScalarImageToTextureFeaturesFilter
+    <RealImageType, HistogramFrequencyContainerType> TextureFilterType;
+  typename TextureFilterType::Pointer textureFilter = TextureFilterType::New();
+  textureFilter->SetInput( imageReader->GetOutput() );
 
-  unsigned int nOffsets = atoi( argv[3] );
-  for( unsigned int n = 0; n < nOffsets; n++ )
-    {
-    typename CooccurrenceMatrixGeneratorType::OffsetType offset;
-    std::vector<int> vector
-      = ConvertVector<int>( std::string( argv[4 + n] ) );
-    if( vector.size() > 0 && vector.size() != ImageDimension )
-      {
-      std::cerr << "Error:  offset size does not equal image dimension." << std::endl;
-      return EXIT_FAILURE;
-      }
-    else
-      {
-      for ( unsigned int d = 0; d < ImageDimension; d++ )
-        {
-        offset[d] = vector[d];
-        }
-      }
-    offsets->push_back( offset );
-    }
-  generator->SetOffsets( offsets );
-
-
-  typename ImageType::Pointer mask = ImageType::New();
-  if ( argc > static_cast<int>( 4 + nOffsets ) )
+  typename ImageType::Pointer mask = NULL;
+  PixelType label = itk::NumericTraits<PixelType>::One;
+  if ( argc > 4 )
     {
     typename ReaderType::Pointer labelImageReader = ReaderType::New();
-    labelImageReader->SetFileName( argv[4 + nOffsets] );
+    labelImageReader->SetFileName( argv[4] );
     labelImageReader->Update();
     mask = labelImageReader->GetOutput();
-    }
-  else
-    {
-    mask->SetOrigin( imageReader->GetOutput()->GetOrigin() );
-    mask->SetSpacing( imageReader->GetOutput()->GetSpacing() );
-    mask->SetRegions( imageReader->GetOutput()->GetLargestPossibleRegion() );
-    mask->Allocate();
-    mask->FillBuffer( itk::NumericTraits<PixelType>::Zero );
-    }
-  PixelType label = itk::NumericTraits<PixelType>::One;
-  if ( argc >static_cast<int>( 5 + nOffsets ) )
-    {
-    label = static_cast<PixelType>( atoi( argv[5 + nOffsets] ) );
+    textureFilter->SetInput( mask );
+
+    if ( argc > 5 )
+      {
+      label = static_cast<PixelType>( atoi( argv[5] ) );
+      }
+    textureFilter->SetInsidePixelValue( label );
     }
 
-  itk::ImageRegionIterator<ImageType> ItI( imageReader->GetOutput(),
+
+  unsigned int numberOfBins = 256;
+  if ( argc > 3 )
+    {
+    numberOfBins = static_cast<PixelType>( atoi( argv[3] ) );
+    }
+  textureFilter->SetNumberOfBinsPerAxis( numberOfBins );
+
+
+  itk::ImageRegionIteratorWithIndex<ImageType> ItI( imageReader->GetOutput(),
     imageReader->GetOutput()->GetLargestPossibleRegion() );
-  itk::ImageRegionIterator<ImageType> ItM( mask,
-    mask->GetLargestPossibleRegion() );
 
-  PixelType maxValue = itk::NumericTraits<PixelType>::min();
+  PixelType maxValue = itk::NumericTraits<PixelType>::NonpositiveMin();
   PixelType minValue = itk::NumericTraits<PixelType>::max();
 
-  for ( ItM.GoToBegin(), ItI.GoToBegin(); !ItI.IsAtEnd(); ++ItM, ++ItI )
+  for( ItI.GoToBegin(); !ItI.IsAtEnd(); ++ItI )
     {
-    if ( ItM.Get() == label )
+    if ( !mask || ( mask->GetPixel( ItI.GetIndex() ) == label ) )
       {
       if ( ItI.Get() < minValue )
         {
@@ -150,13 +75,11 @@ int GenerateCooccurrenceMeasures( int argc, char *argv[] )
         {
         maxValue = ItI.Get();
         }
-      ItM.Set( itk::NumericTraits<PixelType>::One );
-      }
-    else
-      {
-      ItM.Set( itk::NumericTraits<PixelType>::Zero );
       }
     }
+  textureFilter->SetPixelValueMinMax( minValue, maxValue );
+  textureFilter->SetNumberOfBinsPerAxis( numberOfBins );
+  textureFilter->FastCalculationsOff();
 
   /**
    * Second order measurements
@@ -171,46 +94,49 @@ int GenerateCooccurrenceMeasures( int argc, char *argv[] )
    *   8. haralick's correlation (f8)
    */
 
-  generator->SetInput( imageReader->GetOutput() );
-  generator->SetImageMask( mask );
-  generator->SetNumberOfBinsPerAxis( numberOfBins );
-  generator->SetPixelValueMinMax( minValue, maxValue );
-  generator->Compute();
+  textureFilter->Update();
 
-  typedef itk::Statistics::GreyLevelCooccurrenceMatrixTextureCoefficientsCalculator
-    <typename CooccurrenceMatrixGeneratorType::HistogramType> CalculatorType;
-  typename CalculatorType::Pointer calculator = CalculatorType::New();
-  calculator->SetHistogram( generator->GetOutput() );
-  calculator->Compute();
+  typename TextureFilterType::FeatureValueVectorPointer means =
+    textureFilter->GetFeatureMeans();
+  const typename TextureFilterType::FeatureNameVector* names =
+    textureFilter->GetRequestedFeatures();
 
-  RealType energy = calculator->GetEnergy();
-  RealType entropy = calculator->GetEntropy();
-  RealType correlation = calculator->GetCorrelation();
-  RealType inverseDifferenceMoment = calculator->GetInverseDifferenceMoment();
-  RealType inertia = calculator->GetInertia();
-  RealType clusterShade = calculator->GetClusterShade();
-  RealType clusterProminence = calculator->GetClusterProminence();
-  RealType haralickCorrelation = calculator->GetHaralickCorrelation();
+  typename TextureFilterType::FeatureValueVector::ConstIterator mIt =
+    means->Begin();
+  typename TextureFilterType::FeatureNameVector::ConstIterator nIt =
+    names->Begin();
 
-  std::cout << "[" << argv[0] << "]" << std::endl;
-  std::cout << energy << " "
-            << entropy << " "
-            << correlation << " "
-            << inverseDifferenceMoment << " "
-            << inertia << " "
-            << clusterShade << " "
-            << clusterProminence << " "
-            << haralickCorrelation << std::endl;
+
+  std::cout << "energy, entropy, inverse difference moment, inertia, cluster shade, cluster prominence" << std::endl;
+  while( mIt != means->End() )
+    {
+//    std::cout << nIt.Value() << ": " << mIt.Value() << std::endl;
+    std::cout << mIt.Value() << " ";
+    ++mIt;
+    ++nIt;
+    }
+  std::cout << std::endl;
+
+//  RealType entropy = textureFilter->GetFeatureMeansOutput()[1];
+//  RealType inverseDifferenceMoment = textureFilter->GetFeatureMeansOutput()[2];
+//  RealType inertia = textureFilter->GetFeatureMeansOutput()[3];
+//  RealType clusterShade = textureFilter->GetFeatureMeansOutput()[4];
+//  RealType clusterProminence = textureFilter->GetFeatureMeansOutput()[5];
+//
+//  std::cout << energy << " "
+//            << entropy << " "
+//            << inverseDifferenceMoment << " "
+//            << inertia << " "
+//            << clusterShade << " "
+//            << clusterProminence << std::endl;
 
 /*
   std::cout << "energy             : " << energy << std::endl;
   std::cout << "entropy            : " << entropy << std::endl;
-  std::cout << "correlation        : " << correlation << std::endl;
   std::cout << "inverse diff moment: " << inverseDifferenceMoment << std::endl;
   std::cout << "inertia            : " << inertia << std::endl;
   std::cout << "cluster Shade      : " << clusterShade << std::endl;
   std::cout << "cluster prominence : " << clusterProminence << std::endl;
-  std::cout << "haralickCorrelation: " << haralickCorrelation << std::endl;
 */
 
   return 0;
@@ -222,7 +148,7 @@ int main( int argc, char *argv[] )
   if ( argc < 4 )
     {
     std::cout << "Usage: " << argv[0] << " imageDimension inputImage "
-     << "nOffsets [offset1] [offset2] ... [offsetn]  [labelImage] [label]" << std::endl;
+     << "[numberOfBinsPerAxis=256] [maskImage] [maskLabel=1]" << std::endl;
     exit( 1 );
     }
 
