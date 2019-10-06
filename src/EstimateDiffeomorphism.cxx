204d041940ef94e400e3eae5efcbc32b9596c870
Nick Tustison <ntustison@gmail.com>
2019-10-05 22:16:59 -0700

    ENH:  Reverse order.

--- src/EstimateDiffeomorphism.cxx
+++ src/EstimateDiffeomorphism.cxx
@@ -423,7 +423,7 @@ int main(int argc, char *argv[])
   if(argc < 4)
     {
     std::cout << argv[0] << " imageDimension inputDisplacementField outputPrefix "
-                         << " [totalFieldSmoothingVariance=0.0] [updateFieldSmoothingVariance=0.0] "
+                         << " [updateFieldSmoothingVariance=0.0] [totalFieldSmoothingVariance=0.0] "
                          << " [numberOfIterations=100] [learningRate=0.1] [convergenceThreshold=0.0]" << std::endl;
     exit(0);
     }
